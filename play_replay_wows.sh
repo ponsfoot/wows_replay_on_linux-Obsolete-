4bbaac16effa8377c415aea6473a0e9b7f728ab4
hashimo <cabezon.hashimoto@gmail.com>
2018-11-26 17:52:38 +0900

    cleanup

--- play_replay_wows.sh
+++ play_replay_wows.sh
@@ -2,5 +2,5 @@
 
 replayfile="`basename $1`"
 
-env WINEPREFIX="${HOME}/.wine32" WINARCH=win32 /usr/bin/wine C:\\windows\\command\\start.exe /Unix ${HOME}/.wine32/dosdevices/c:/Games/World_of_Warships/WorldOfWarships.exe "C:\\Games\\World_of_Warships\\replays\\$replayfile"
+env WINEPREFIX="${HOME}/.wine32" /usr/bin/wine C:\\windows\\command\\start.exe /Unix ${HOME}/.wine32/dosdevices/c:/Games/World_of_Warships/WorldOfWarships.exe "C:\\Games\\World_of_Warships\\replays\\$replayfile"
 
