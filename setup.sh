99b252b2b112a514b6734b1251a17be8b31e83b0
hashimo <cabezon.hashimoto@gmail.com>
2018-11-26 23:37:42 +0900

    more reliable

--- setup.sh
+++ setup.sh
@@ -9,13 +9,14 @@ fi
 
 basedir=`dirname "$(readlink -f $0)"`
 menudir=`ls -d "${XDG_DATA_HOME}/applications/wine/Programs/World of Warships"* | head -n1`
-wineprefix=`sed -n 's/.*WINEPREFIX="\(.\+\)".*/\1/p' ${menudir}/World\ of\ Warships.desktop`
-wowspath=`sed -n 's/Path=\(.\+\)/\1/p' ${menudir}/World\ of\ Warships.desktop`
-wowstop=`basename $wowspath`
+wineprefix=`sed -n 's/.*WINEPREFIX="\(.\+\)".*/\1/p' "${menudir}/World of Warships.desktop"`
+linuxpath=`sed -n 's/Path=\(.\+\)/\1/p' "${menudir}/World of Warships.desktop"`
+winpath=`sed -n 's|.*dosdevices/\(.\+\)|\1|p' <<< "$linuxpath"`
+winpath=${winpath//\//\\\\\\\\}
 launcher="${basedir}/play_replay_wows.sh"
 
-sed "s|@WINEPREFIX@|${wineprefix}|g;s|@WOWSTOP@|${wowstop}|g" "${basedir}/play_replay_wows.sh.templete" > "$launcher"
-sed "s|@LAUNCHER@|${launcher}|g;s|@WOWSPATH@|${wowspath}|g" "${basedir}/wows_replay.desktop.template" > "${XDG_DATA_HOME}/applications/wows_replay.desktop"
+sed "s|@WINEPREFIX@|${wineprefix}|g;s|@LINUXPATH@|${linuxpath}|g;s|@WINPATH@|${winpath}|g" "${basedir}/play_replay_wows.sh.templete" > "$launcher"
+sed "s|@LAUNCHER@|${launcher}|g;s|@LINUXPATH@|${linuxpath}|g" "${basedir}/wows_replay.desktop.template" > "${XDG_DATA_HOME}/applications/wows_replay.desktop"
 cp -af "${basedir}/x-wine-wows-replay.xml" "${XDG_DATA_HOME}/mime/packages/"
 
 update-mime-database "${XDG_DATA_HOME}/mime/"
