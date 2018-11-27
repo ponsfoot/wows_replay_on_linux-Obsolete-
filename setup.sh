1537bc8d060404cb1431d502d1bed00702a56336
hashimo <cabezon.hashimoto@gmail.com>
2018-11-27 14:34:04 +0900

    install play_replay_wows.sh into same directory with wows

--- setup.sh
+++ setup.sh
@@ -3,23 +3,21 @@
 IFS='
 '
 
-if [[ -z $XDG_DATA_HOME ]]; then
-    XDG_DATA_HOME="${HOME}/.local/share"
-fi
+xdg_data_home="${XDG_DATA_HOME:-${HOME}/.local/share}"
 
 basedir=`dirname "$(readlink -f $0)"`
-menudir=`ls -d "${XDG_DATA_HOME}/applications/wine/Programs/World of Warships"* | head -n1`
+menudir=`ls -d "${xdg_data_home}/applications/wine/Programs/World of Warships"* | head -n1`
 wineprefix=`sed -n 's/.*WINEPREFIX="\(.\+\)".*/\1/p' "${menudir}/World of Warships.desktop"`
 linuxpath=`sed -n 's/Path=\(.\+\)/\1/p' "${menudir}/World of Warships.desktop"`
 winpath=`sed -n 's|.*dosdevices/\(.\+\)|\1|p' <<< "$linuxpath"`
 winpath=${winpath//\//\\\\\\\\}
-launcher="${basedir}/play_replay_wows.sh"
+launcher="${linuxpath%/*}/play_replay_wows.sh"
 
 sed "s|@WINEPREFIX@|${wineprefix}|g;s|@LINUXPATH@|${linuxpath}|g;s|@WINPATH@|${winpath}|g" "${basedir}/play_replay_wows.sh.template" > "$launcher"
-sed "s|@LAUNCHER@|${launcher}|g;s|@LINUXPATH@|${linuxpath}|g" "${basedir}/wows_replay.desktop.template" > "${XDG_DATA_HOME}/applications/wows_replay.desktop"
-cp -af "${basedir}/x-wine-wows-replay.xml" "${XDG_DATA_HOME}/mime/packages/"
+sed "s|@LAUNCHER@|${launcher}|g;s|@LINUXPATH@|${linuxpath}|g" "${basedir}/wows_replay.desktop.template" > "${xdg_data_home}/applications/wows_replay.desktop"
+cp -af "${basedir}/x-wine-wows-replay.xml" "${xdg_data_home}/mime/packages/"
 
-update-mime-database "${XDG_DATA_HOME}/mime/"
-update-desktop-database "${XDG_DATA_HOME}/applications/"
+update-mime-database "${xdg_data_home}/mime/"
+update-desktop-database "${xdg_data_home}/applications/"
 
 chmod +x "$launcher"
