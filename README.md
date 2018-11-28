59ebaf0faf54e30a85df637df9689a78ab4888a7
hashimo <cabezon.hashimoto@gmail.com>
2018-11-28 17:10:49 +0900

    support WoT

--- README.md
+++ README.md
@@ -1,25 +1,34 @@
-# Play WoWS replay files on Linux
+# Play WoT & WoWS replay files on Linux
 
 ## Install
-run `setup.sh` on terminal.
+1. Clone git repo or download and extracct zip file
+2. run `setup.sh`
 
 ## How to play replays
-Replay files are stored in `${WINEPREFIX}/dosdevices/c:/Games/World_of_Warships (or your own custom)/replays/`
+Replay files are stored in `${WINEPREFIX}/dosdevices/c:/Games/World_of_{Tanks,Warships}* (or your own custom)/replays/`
 
-1. Right click on a replay file (*.wowsreplay)
-2. Choose "Open with World of Warships play replay"
+1. Right click on a replay file (\*.wotreplay/\*.wowsreplay)
+2. Choose "Open with World of Tanks/Warships play replay"
 
 ## What files are installed
+- ${XDG_DATA_HOME}/mime/packages/x-wine-wot-replay.xml
 - ${XDG_DATA_HOME}/mime/packages/x-wine-wows-replay.xml
-	- Add MIME-type for replay file named "application/x-wine-wows-replay"
+- ${XDG_DATA_HOME}/applications/wot_replay.desktop
 - ${XDG_DATA_HOME}/applications/wows_replay.desktop
-	- .desktop file to assagin replay files to the following launch script
+- ${WINEPREFIX}/ *\<Path WoT installed \>* /play_replay_wowt.sh
 - ${WINEPREFIX}/ *\<Path WoWS installed \>* /play_replay_wows.sh
-	- Shell script to launch WoWS with replay file which modified to Windows file path
 
 if $XDG_DATA_HOME is not set, default is equal to `${HOME}/.local/share`.
 
+## Uninsall
+Remove files the above and run the following commands.
+
+```
+$ update-mime-database "${XDG_DATA_HOME:-${HOME}/.local/share}/mime/"
+$ update-desktop-database "${XDG_DATA_HOME:-${HOME}/.local/share}/applications/"
+```
+
 ## Note
 - WGC is not supported now.
-- Replay files have to be placed under Wine drive configuration (can be described as Windows file path) and same $WINEPREFIX with WoWS installed.
-- If you have installed WoWS plural (e.g. WoWS_NA and WoWS_RU), first one sorted by name will be chosen as replay environment.
+- Replay files have to be placed under Wine drive configuration (can be described as Windows file path) and same $WINEPREFIX with WoT/WoWS installed.
+- If you have installed WoT/WoWS plural (e.g. WoWS_NA and WoWS_RU), first one sorted by name will be chosen as replay environment.
--- setup.sh
+++ setup.sh
@@ -6,16 +6,41 @@ IFS='
 xdg_data_home="${XDG_DATA_HOME:-${HOME}/.local/share}"
 
 basedir=`dirname "$(readlink -f $0)"`
-menudir=`ls -d "${xdg_data_home}/applications/wine/Programs/World of Warships"* | head -n1`
-wineprefix=`sed -n 's/.*WINEPREFIX="\(.\+\)".*/\1/p' "${menudir}/World of Warships.desktop"`
-wowspath=`sed -n 's/Path=\(.\+\)/\1/p' "${menudir}/World of Warships.desktop"`
-launcher="${wowspath%/*}/play_replay_wows.sh"
 
-sed "s|@WINEPREFIX@|${wineprefix}|g;s|@WOWSPATH@|${wowspath}|g" "${basedir}/play_replay_wows.sh.template" > "$launcher"
-sed "s|@LAUNCHER@|${launcher}|g;s|@WOWSPATH@|${wowspath}|g" "${basedir}/wows_replay.desktop.template" > "${xdg_data_home}/applications/wows_replay.desktop"
-cp -af "${basedir}/x-wine-wows-replay.xml" "${xdg_data_home}/mime/packages/"
+gamename=("World of Warships" "World of Tanks")
+gameabbr=(wows wot)
+gameicon=(F66B_WoWSLauncher.0 7680_WoTLauncher.0)
+gameexe=(WorldOfWarships.exe WorldOfTanks.exe)
+
+for i in 0 1
+do
+    name=${gamename[i]}
+    abbr=${gameabbr[i]}
+    icon=${gameicon[i]}
+    exe=${gameexe[i]}
+
+    menudir=`ls -d "${xdg_data_home}/applications/wine/Programs/${name}"* | head -n1`
+    wineprefix=`sed -n 's/.*WINEPREFIX="\(.\+\)".*/\1/p' "${menudir}/${name}.desktop"`
+    gamepath=`sed -n 's/Path=\(.\+\)/\1/p' "${menudir}/${name}.desktop"`
+    launcher="${gamepath%/*}/play_replay_${abbr}.sh"
+
+    sed -e "s|@WINEPREFIX@|${wineprefix}|g" \
+        -e "s|@GAMEPATH@|${gamepath}|g" \
+        -e "s|@ICON@|${icon}|g" \
+        -e "s|@EXE@|${exe}|g" \
+        "${basedir}/play_replay_wows.sh.template" > "$launcher"
+    sed -e "s|@NAME@|${name}|g" \
+        -e "s|@LAUNCHER@|${launcher}|g" \
+        -e "s|@ABBR@|${abbr}|g" \
+        -e "s|@GAMEPATH@|${gamepath}|g" \
+        -e "s|@ICON@|${icon}|g" \
+        "${basedir}/wows_replay.desktop.template" > "${xdg_data_home}/applications/${abbr}_replay.desktop"
+    sed -e "s|@ABBR@|${abbr}|g" \
+        -e "s|@NAME@|${name}|g" \
+        "${basedir}/x-wine-wows-replay.xml.template" > "${xdg_data_home}/mime/packages/x-wine-${abbr}-replay.xml"
+
+    chmod +x "$launcher"
+done
 
 update-mime-database "${xdg_data_home}/mime/"
 update-desktop-database "${xdg_data_home}/applications/"
-
-chmod +x "$launcher"
